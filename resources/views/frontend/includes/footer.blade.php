f0390e13fcb685898ae5c4e7e61022f979b6eb1e
Din Mohammad <dinmohammad.bd2024@gmail.com>
2025-07-24 21:07:14 +0600

    footer section add

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -1,1 +1,69 @@
-<h1>Footer</h1>
+<Footer class="footer-wrapper position-relative overflow-hidden section-padding-t-70">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-5 col-md-5 col-sm-6 col-12">
+                <div class="section-heading">
+                    <h2 class="text-white">We’d love to hear <br> from you</h2>
+                    <p class="mt-3 mb-5  text-white"> 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    </p>
+                    <small class="text-white f-get-text">GET IN TOUCH</small>
+                    <p class="text-white f-p-text mt-4 mb-0">211B Thornridge Cir. Syracuse, Connecticut 35624</p>
+                    <p class="text-white f-p-text">(208) 555-0112</p>
+                </div>
+            </div>
+
+            <div class="col-xl-7 col-md-7 col-sm-6 col-12">
+                <div class="f-contact-us-wrap">
+                    <div class="section-heading">
+                        <h2>Contact Us</h2>
+                    </div>
+                    <div class="contact-form-wrap section-content-top-margin">
+                        <form >
+                            <div class="row g-4">
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" placeholder="First name" />
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" placeholder="Last name" />
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="email" class="form-control" placeholder="Email" />
+                                </div>
+                                <div class="col-md-6">
+                                    <input type="text" class="form-control" placeholder="Phone Number" />
+                                </div>
+                                <div class="col-12">
+                                    <textarea class="form-control" rows="4" placeholder="Message"></textarea>
+                                </div>
+                                <div class="col-12">
+                                    <button type="submit" class="theme-primary-solid-button">Submit</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="bottom-footer section-padding-t-70">
+            <div class="row">
+                <div class="col-xl-5 col-md-5 col-sm-6 col-12">
+                    <div class="social-links d-flex gap-4 justify-content-center align-items-center">
+                        <a href="#">INSTAGRAM</a>
+                        <a href="#">FACEBOOK</a>
+                        <a href="#">TWITTER</a>
+                    </div>
+                </div>
+                <div class="col-xl-7 col-md-7 col-sm-6 col-12">
+                    <div class="bottom-footer-menu d-flex flex-wrap  gap-4 justify-content-start">
+                        <a href="#">Home</a>
+                        <a href="#">About Us</a>
+                        <a href="#">Blog</a>
+                        <a href="#">Contact Us</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</Footer>
